16fd68c9506da8cd0d8e972f9157fc0ae4dfab2b
L2ncE <llance_24@foxmail.com>
2022-04-10 16:57:02 +0800

    refactor

--- hub.go
+++ hub.go
@@ -2,147 +2,124 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
-	"log"
-	"net/http"
-	"strings"
-	"time"
 )
 
-var (
-	upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
-)
-
-func (m message) readPump() {
-	c := m.conn
-
-	defer func() {
-		h.unregister <- m
-		c.ws.Close()
-	}()
-
-	c.ws.SetReadLimit(maxMessageSize)
-	c.ws.SetReadDeadline(time.Now().Add(pongWait))
-	c.ws.SetPongHandler(func(string) error {
-		c.ws.SetReadDeadline(time.Now().Add(pongWait))
-		return nil
-	})
-
-	for {
-		_, msg, err := c.ws.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
-				fmt.Println("err:", err)
-			}
-			break
-		}
-		go m.Limit(msg)
-	}
-}
-
-// Limit 发言违规进行时间限制禁言,超过三次禁言
-func (m message) Limit(msg []byte) {
-	c := m.conn
-	//查看是否禁言，并判断是否超过了禁言时间
-	timeNow := time.Now().Unix()
-	if timeNow-c.timeLog < 300 {
-		h.warnmsg <- m
-	}
-
-	// 不合法信息3次，判断是否有不合法信息，没有进行信息发布
-	if c.limitNum >= 3 {
-		h.kickoutroom <- m
-		log.Println("素质太低，给你踢出去")
-		c.ws.Close() //
-	} else //没有超过三次，可以继续
-	{
-		baseStr := "死逼批操" //违法字符
-		testStr := string(msg[:])
-		for _, word := range testStr {
-			//遍历是否有违法字符
-			res := strings.Contains(baseStr, string(word))
-			if res == true {
-				c.limitNum += 1
-				c.forbiddenWord = true //禁言
-				//记录禁言开始时间
-				c.timeLog = time.Now().Unix()
-				h.warnings <- m
-				break
-			}
-		}
-		// 不禁言，消息合法 可以发送
-		if c.forbiddenWord != true {
-			// 通过所有检查，进行广播
-
-			if msg[0] == 48 {
-				head := "所有玩家请注意:"
-				data := head + string(msg[1:])
-				m := message{[]byte(data), m.roomid, c}
-				h.broadcastss <- m
-			} else if msg[0] != 48 { //不是0，就是普通消息
-				m := message{msg, m.roomid, c}
-				h.broadcast <- m
-			}
-		}
-	}
-}
-
-func (c *connection) write(mt int, payload []byte) error {
-	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
-	return c.ws.WriteMessage(mt, payload)
-}
-
-func (s *message) writePump() {
-	c := s.conn
-
-	ticker := time.NewTicker(pingPeriod)
-
-	defer func() {
-		ticker.Stop()
-		c.ws.Close()
-	}()
-
+func (h *hub) run() {
 	for {
 		select {
-		case message, ok := <-c.send:
-			if !ok {
-				c.write(websocket.CloseMessage, []byte{})
-				return
+		case m := <-h.register: //传输链接
+			conns := h.rooms[m.roomid]
+			if conns == nil { // 链接保存到相应的房间
+				conns = make(map[*connection]bool)
+				h.rooms[m.roomid] = conns
+				fmt.Println("在线人数:==", len(conns))
+				fmt.Println("rooms:==", h.rooms)
 			}
-			if err := c.write(websocket.TextMessage, message); err != nil {
-				return
+			h.rooms[m.roomid][m.conn] = true
+			fmt.Println("在线人数:==", len(conns))
+			fmt.Println("rooms:==", h.rooms)
+
+			for con := range conns {
+				sysmsg := "系统消息：欢迎新伙伴加入" + m.roomid + "聊天室！！！"
+				data := []byte(sysmsg)
+				select {
+				case con.send <- data:
+				}
 			}
-		case <-ticker.C:
-			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
-				return
+
+		case m := <-h.unregister: //断开链接
+			conns := h.rooms[m.roomid]
+			if conns != nil {
+				if _, ok := conns[m.conn]; ok {
+					delete(conns, m.conn) //删除链接
+					close(m.conn.send)
+					for con := range conns {
+						delmsg := "系统消息：有小伙伴离开了" + m.roomid + "聊天室"
+						data := []byte(delmsg)
+						select {
+						case con.send <- data:
+						}
+						if len(conns) == 0 { // 链接都断开，删除房间
+							delete(h.rooms, m.roomid)
+						}
+					}
+				}
 			}
+
+		case m := <-h.kickoutroom: //3次不合法信息后，被踢出群聊
+			conns := h.rooms[m.roomid]
+			notice := "由于您多次发送不合法信息,已被踢出群聊！！！"
+			select {
+			case m.conn.send <- []byte(notice):
+			}
+			if conns != nil {
+				if _, ok := conns[m.conn]; ok {
+					delete(conns, m.conn)
+					close(m.conn.send)
+					if len(conns) == 0 {
+						delete(h.rooms, m.roomid)
+					}
+				}
+			}
+
+		case m := <-h.warnings: //不合法信息警告
+			conns := h.rooms[m.roomid]
+			if conns != nil {
+				if _, ok := conns[m.conn]; ok {
+					notice := "警告:您发布不合法信息，将禁言5分钟，三次后将被踢出群聊！！！"
+					select {
+					case m.conn.send <- []byte(notice):
+					}
+				}
+			}
+
+		case m := <-h.warnmsg: //禁言中提示
+			conns := h.rooms[m.roomid]
+			if conns != nil {
+				if _, ok := conns[m.conn]; ok {
+					notice := "您还在禁言中,暂时不能发送信息！！！"
+					select {
+					case m.conn.send <- []byte(notice):
+					}
+				}
+			}
+
+		case m := <-h.broadcast: //传输群信息/房间信息
+			conns := h.rooms[m.roomid]
+			for con := range conns {
+				if con == m.conn { //自己发送的信息，不用再发给自己
+					continue
+				}
+				select {
+				case con.send <- m.data:
+				default:
+					close(con.send)
+					delete(conns, con)
+					if len(conns) == 0 {
+						delete(h.rooms, m.roomid)
+					}
+				}
+			}
+
+		case m := <-h.broadcastss: //传输全员广播信息
+			for _, conns := range h.rooms {
+				for con := range conns {
+					if con == m.conn { //自己发送的信息，不用再发给自己
+						continue
+					}
+					select {
+					case con.send <- m.data:
+					default:
+						close(con.send)
+						delete(conns, con)
+						if len(conns) == 0 {
+							delete(h.rooms, m.roomid)
+						}
+					}
+				}
+
+			}
+
 		}
 	}
-}
-
-func serverWs(ctx *gin.Context) {
-	ctx.Request.ParseForm()
-	roomid := ctx.Request.Form["roomid"][0]
-
-	ws, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
-
-	if err != nil {
-		fmt.Println("err:", err)
-		return
-	}
-
-	c := &connection{send: make(chan []byte, 256), ws: ws}
-	m := message{nil, roomid, c}
-
-	h.register <- m
-
-	go m.writePump()
-	go m.readPump()
 }
