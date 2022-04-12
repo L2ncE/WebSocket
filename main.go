6437a90936931c8e8d3820e1035b193034e344a5
L2ncE <llance_24@foxmail.com>
2022-04-12 17:00:02 +0800

    refactor:重构主函数

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
-	go h.run()
+	err := InitGormDB()
+	if err != nil {
+		fmt.Printf("init redis failed, err:%v\n", err)
+	} else {
+		fmt.Println("连接GORM MySQL数据库成功!")
+	}
 	InitEngine()
 }
